852f2ae49d1bc9968a44d553b6ba251bc2c4d9fc
Ervis <efrend721@gmail.com>
2024-10-13 12:40:41 -0600

    Deleted coments

--- templates/addnoteprojectlist.template.php
+++ templates/addnoteprojectlist.template.php
@@ -12,37 +12,7 @@ echo "<main class='mt-5 pt-3'>
                 
                 include_once '../templates/carddetailproject.template.php';
             }
-            //require_once ('../includes/mensages.php');
-            
-            /*echo "<div class='card'>
-                <div class='card-header'>
-                    <span><i class='bi bi-table me-2'></i></span> Project <span id='idJob'>$idjob</span>
-                </div>
-            ";
-            include_once '../queries/projectlist.query.php';
-            $stmt1 = $dbh->connect()->prepare($projectlistquery['details']['specificProject']);
-            $stmt1->bindParam(1, $idjob , PDO::PARAM_INT);
-            $stmt1->execute();
-            echo "<div class='card-body'>
-                    <div class='shadow row mb-2 p-2 bg-dark border rounded-3 text-white'>
-                        <div class='col-6'><span class='strong'>Address</span></div>
-                        <div class='col-2'>Date</div>
-                        <div class='col-2'>Piece</div>
-                        <div class='col-2'>Color</div>
-                    </div>
-                    <div class='shadow row mb-2 p-3 bg-secondary bg-opacity-50 border rounded-3 '>";
-                        $results = $stmt1->fetchAll(PDO::FETCH_ASSOC);
-                        foreach($results as $result) {
-                        echo "<div class='col-6 text-truncate'>$result[address]</div>
-                                <div class='col-2'>$result[date]</div>
-                                <div class='col-2'>$result[piece]</div>
-                                <div class='col-2'>$result[id_color]</div>";
-                        }
-                
-                    echo "</div>
-                </div>
-            </div> 
-            <br>*/
+           
             echo "<form action='' method='POST'>
                 <div class='card'>
                     
